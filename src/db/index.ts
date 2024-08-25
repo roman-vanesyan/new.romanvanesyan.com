8ee20ce21fdf5e98338f709dcd80ab3870801de6
Roman Vanesyan <roman@vanesyan.com>
2024-08-26 02:11:54 +0300

    chore: mb this? (2)

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client/edge';
 import { PrismaD1 } from '@prisma/adapter-d1';
 
 export default function create_db(client: Env['DB']) {
