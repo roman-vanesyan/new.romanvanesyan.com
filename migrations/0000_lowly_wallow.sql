6a31c2ade0c4571d1ba9e30f4eb600119315f5b8
Roman Vanesyan <roman@vanesyan.com>
2024-08-26 17:26:54 +0300

    fix: db & header css

--- migrations/0000_lowly_wallow.sql
+++ migrations/0000_lowly_wallow.sql
@@ -1,5 +1,6 @@
 CREATE TABLE `subscribed_users` (
-	`id` integer DEFAULT CURRENT_TIMESTAMP,
+	`id` integer PRIMARY KEY NOT NULL,
+	`created_at` integer DEFAULT CURRENT_TIMESTAMP,
 	`email` text NOT NULL,
 	`name` text,
 	`is_email_verified` integer DEFAULT false NOT NULL
--- migrations/0001_add_created_at_subscribed_users.sql
+++
@@ -1,2 +0,0 @@
---> statement-breakpoint
-ALTER TABLE `subscribed_users` ADD `created_at` integer DEFAULT CURRENT_TIMESTAMP;
